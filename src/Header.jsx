fc3d7ec85d8d9db23a55923a14e867f87f1b0d76
Dev Sharma <devsharmaelc@gmail.com>
2024-08-16 18:27:08 +0530

    some chage

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="header">
       <div>
         <NavLink to="/login">Login</NavLink>
-        <NavLink to="/registration">Users</NavLink>
+        <NavLink to="/">All Users</NavLink>
       </div>
     </div>
   );
